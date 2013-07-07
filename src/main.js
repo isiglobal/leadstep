e084fad7efcb4c4f20d10fb87a9211340cbfad88
Brandon Thomas <bt@brand.io>
2013-07-06 22:12:03 -0400

    requirejs config

---
+++ src/main.js
@@ -0,0 +1,15 @@
+define(['jquery', 'underscore', 'backbone'], function(a, b, c) {
+	'use strict';
+
+	console.log('loaded main');
+	console.log('jquery:', a);
+	console.log('underscore:', b);
+	console.log('backbone:', c);
+
+	return {};
+	/*return {
+		foo: function() {
+			console.log('foo');
+		},
+	}*/
+});
